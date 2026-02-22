28170e8ce177b41035b53a082e58e5d22e9e1205
yogi99k <yogikoteru99@gmail.com>
2026-02-21 22:26:47 -0500

    RequestMapping

--- backend/src/main/java/com/eazybytes/backend/controller/DemoController.java
+++ backend/src/main/java/com/eazybytes/backend/controller/DemoController.java
@@ -1,11 +1,14 @@
 package com.eazybytes.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
     @GetMapping("/home")
+    @RequestMapping(path="/home",method = {RequestMethod.GET,RequestMethod.POST})
     public String demo() {
         return "Hello, World!";
     }
--- backend/src/main/java/com/eazybytes/backend/controller/UserController.java
+++ backend/src/main/java/com/eazybytes/backend/controller/UserController.java
@@ -2,14 +2,16 @@ package com.eazybytes.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/dummy/users/")
 public class UserController {
 
-    @GetMapping({"/api/dummy/users/{userId}/posts", "/api/dummy/users/{userId}/posts/{postId}"})
+    @GetMapping({"{userId}/posts", "{userId}/posts/{postId}"})
     public String searchUserPostWithMultiPathVariables(@PathVariable("userId") Long userId,@PathVariable (value="postId", required = false) Long postId) {
         if (postId == null) {
             return "Fetching all posts for user with id " + userId;
@@ -17,7 +19,7 @@ public class UserController {
         return "Fetching post with id " + postId + " for user with id " + userId;
     }
 
-    @GetMapping({"/api/dummy/users/{userId}/orders/{orderId}","/api/dummy/users/{userId}"})
+    @GetMapping({"{userId}/orders/{orderId}","{userId}"})
     public String searchUserOrderWithMultiPathVariables(@PathVariable(name="userId") Long customerId,
                                                         @PathVariable(required = false) Long orderId){
         return "Fetching post with id " + orderId + " for user with id " + customerId;
@@ -28,7 +30,7 @@ public class UserController {
      * @param pathVariablesMap
      * @return
      */
-    @GetMapping("/api/dummy/users/{userId}/address/{customerId}")
+    @GetMapping("{userId}/address/{customerId}")
     public String searchUserAddressrWithMultiPathVariables(@PathVariable Map<String,String> pathVariablesMap){
         return "Fetching post with id " + pathVariablesMap.get("userId") + " for user with id " + pathVariablesMap.get("customerId");
     }
