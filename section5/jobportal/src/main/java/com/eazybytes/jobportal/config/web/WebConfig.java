db3c55c9003350219b33fd59b87fdf314122265c
yogi99k <yogikoteru99@gmail.com>
2026-02-23 19:01:33 -0500

    Section 5 (Cors) 4/n

--- section5/jobportal/src/main/java/com/eazybytes/jobportal/config/web/WebConfig.java
+++ section5/jobportal/src/main/java/com/eazybytes/jobportal/config/web/WebConfig.java
@@ -2,7 +2,9 @@ package com.eazybytes.jobportal.config.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -35,5 +37,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
         configurer.addPathPrefix("/api",_ -> true);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
     }
 }
